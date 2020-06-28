c1e566de45d769a287dafa02fad009498bf23208
lin pascal <dunlintan@hk01.com>
2020-06-28 18:12:52 +0800

    refactor: set up ws server

--- main.go
+++ main.go
@@ -4,33 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	Controllers "github.com/pascallin/go-communication/controllers"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.ServeFile(w, r, "home.html")
-}
+var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
 	flag.Parse()
-	hub := newHub()
-	go hub.run()
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(hub, w, r)
-	})
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	log.SetFlags(0)
+	http.HandleFunc("/echo", Controllers.Echo)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
